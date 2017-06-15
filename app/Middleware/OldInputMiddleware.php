9c4f761350ec6d6ac1c84cfb017ead269d263ac2
Kevin Köllmann <kevinkoellmann@outlook.com>
2017-06-15 15:20:44 +0200

    Keep old input on navigating to the same page again

--- app/Middleware/OldInputMiddleware.php
+++ app/Middleware/OldInputMiddleware.php
@@ -9,9 +9,32 @@ class OldInputMiddleware extends Middleware
 {
     function __invoke(Request $request, Response $response, $next)
     {
-        $this->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
-        $_SESSION['old'] = $request->getParams();
+        // Navigated to another page, empty the $_SESSION['old'] array
+        if (isset($_SESSION['old_path']) && $_SESSION['old_path'] !== $request->getRequestTarget()) {
+            $_SESSION['old'] = [];
+        }
 
+        // Fill a global view variable with the $_SESSION['old'] contents (if any)
+        if (isset($_SESSION['old'])) {
+            $this->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
+        }
+
+        // Update the $_SESSION['old'] with the current request parameters
+        if (!isset($_SESSION['old'])) {
+            $_SESSION['old'] = $request->getParams();
+        } else {
+            $requestParams = $request->getParams();
+            foreach ($requestParams as $key => $value) {
+                if (isset($_SESSION['old'][$key]) || $_SESSION['old'][$key] !== $value) {
+                    $_SESSION['old'][$key] = $value;
+                }
+            }
+        }
+
+        // Save $_SESSION['old_path'] to the current request target
+        $_SESSION['old_path'] = $request->getRequestTarget();
+
+        // Next Middleware
         $response = $next($request, $response);
         return $response;
     }
