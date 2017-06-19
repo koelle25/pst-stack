a9452b9eef2f1f703ffe52231ab54d449c5c49b0
Kevin Köllmann <mail@kevinkoellmann.de>
2017-06-19 12:04:44 +0200

    Added user creation feature

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use App\Models\UserQuery;
+use App\UUID;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Respect\Validation\Validator as v;
 
 class AdminController extends Controller
 {
@@ -20,5 +23,51 @@ class AdminController extends Controller
         return $this->container->view->render($response, 'admin/users/list.twig', [
             'users' => $users
         ]);
+    }
+
+    public function getNewUser(Request $request, Response $response)
+    {
+        return $this->container->view->render($response, 'admin/users/new.twig');
+    }
+
+    public function postNewUser(Request $request, Response $response)
+    {
+        $validation = $this->validator->validate($request, [
+            'firstName' => v::notEmpty()->alpha('äöüß'),
+            'lastName' => v::notEmpty()->alpha('äöüß'),
+            'email' => v::noWhitespace()->notEmpty()->email()->emailAvailable(),
+        ]);
+
+        if ($validation->failed()) {
+            $this->flash->addMessage('error', 'Please check your input.');
+            return $response->withRedirect($this->router->pathFor('admin.users.new'));
+        }
+
+        $firstName = filter_var($request->getParam('firstName'), FILTER_SANITIZE_STRING);
+        $lastName = filter_var($request->getParam('lastName'), FILTER_SANITIZE_STRING);
+        $emailAddress = strtolower(filter_var($request->getParam('email'), FILTER_SANITIZE_STRING));
+        $password = USER::createPassword();
+
+        $user = new User();
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setEmail($emailAddress);
+        $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
+        $user->setUUID(UUID::v4());
+
+        if ($user->save()) {
+            $this->flash->addMessage('success', 'User creation successful. The password is: <strong>'.$password.'</strong>');
+
+            if ($request->getParam('commit') !== null) {
+                return $response->withRedirect($this->router->pathFor('admin.users'));
+            }
+            elseif ($request->getParam('continue') !== null) {
+                $_SESSION['forget_old_input'] = true;
+                return $response->withRedirect($this->router->pathFor('admin.users.new'));
+            }
+        }
+
+        $this->flash->addMessage('error', 'Sorry, something went wrong');
+        return $response->withRedirect($this->router->pathFor('admin.users.new'));
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -9,5 +9,8 @@ $app->group('/admin', function () use ($container) {
     $this->group('/users', function () {
 
         $this->get('', 'AdminController:getUserList')->setName('admin.users');
+
+        $this->get('/new', 'AdminController:getNewUser')->setName('admin.users.new');
+        $this->post('/new', 'AdminController:postNewUser');
     });
 })->add(new AuthMiddleware($container));
